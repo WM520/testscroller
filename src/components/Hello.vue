49cf15d5eabeff58c02d4c0f83e9cc13886f73e6
WM520 <1071597946@qq.com>
2017-05-18 09:51:03 +0800

    testscroller

---
+++ src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      refreshing: false,
+      loading: false,
+      scroller: null
+      list[{}, {}, {}, {}]
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
